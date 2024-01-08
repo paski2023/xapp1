bb523a570f55c9affb6dd162859c4ff1040568e6
mattia_fiore <mattiafioresocrate@gmail.com>
2024-01-08 20:17:34 +0100

    fix

--- base-xapp/e2sm_proto.py
+++ base-xapp/e2sm_proto.py
@@ -9,12 +9,12 @@ sys.path è una lista di directories dove l'interprete python cerca moduli
 # Si aggiunge il percorso alla cartella Protolib1/builds
 
 path.append('{}/Protolib1/builds'.format(cwd)) 
-import ran_messages_pb2 as tmp
+
 
 # Infatti il file ran_messages pb2.py si trova in quella cartella
-# tmp = __import__('ran_messages_pb2')
+tmp = __import__('ran_messages_pb2')
 # L'import effettuato in questo modo permette di rinominare la libreria
-# Siccome la libreria non cambia nome e mantiene sempre la stessa posizione 
+
 
 globals().update(vars(tmp))
 #Aggiorna le variabili globali con le variabili della libreria ran_messages_pb2
